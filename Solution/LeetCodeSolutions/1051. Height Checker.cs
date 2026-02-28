f3c8ddc2f52e08f1f19fe1850bfb2e337f3baa78
GHOUSTden <ghoust.den2007@gmail.com>
2026-02-28 15:06:55 +0200

    Initial commit

---
+++ Solution/LeetCodeSolutions/1051. Height Checker.cs
@@ -0,0 +1,32 @@
+﻿class Solutions
+{
+    public static int HeightChecker(int[] heights)
+    {
+        int noi = 0;
+        int[] expected = new int[heights.Length];
+
+        Array.Copy(heights, expected, heights.Length);
+
+        Array.Sort(expected);
+
+        for (int i = 0; i < expected.Length; i++)
+        {
+            if (heights[i] != expected[i])
+            {
+                noi++;
+            }
+        }
+
+        Console.WriteLine(noi);
+
+        return noi;
+    }
+
+    public static int Main()
+    {
+        int[] heights = [1, 1, 4, 2, 1, 3];
+        HeightChecker(heights);
+
+        return 1;
+    }
+}
