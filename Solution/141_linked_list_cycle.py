1c5a7bf35a585c5333cf52b3a4f0fb41716a19bf
GHOUSTden <ghoust.den2007@gmail.com>
2026-04-13 18:31:57 +0300

    141 + 142 Solution

---
+++ Solution/141_linked_list_cycle.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+    def __str__(self):
+        return f"{self.val} -> {self.next}"
+
+def has_cycle(head: Node) -> bool:
+    slow_ptr = fast_ptr = head
+    while fast_ptr and fast_ptr.next:
+        slow_ptr = slow_ptr.next
+        fast_ptr = fast_ptr.next.next
+        if slow_ptr == fast_ptr:
+            return True
+
+if __name__ == "__main__":
+    head = Node(1)
+    cycle_node = head.next = Node(2)
+    head.next.next = cycle_node
+
+    print(has_cycle(head))
