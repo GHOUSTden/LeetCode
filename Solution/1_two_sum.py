951bf5097673694ded1cbe749593f10064e9a30f
GHOUSTden <ghoust.den2007@gmail.com>
2026-04-18 20:39:15 +0300

    Two Sum Fix

--- Solution/1_two_sum.py
+++ Solution/1_two_sum.py
@@ -5,10 +5,9 @@ def two_sum(nums: list[int], target: int) -> list[int]:
         for j in range(counter_j, len(nums)):
             if (nums[i] + nums[j]) == target:
                 return [i, j]
-                print([i, j])
 
 if __name__ == "__main__":
-    print(two_sum([2,7,11,15], 9)) == [0, 1]
-    print(two_sum([3,2,4], 6)) == [1,2]
-    print(two_sum([3,3], 6)) == [0,1]
-    print(two_sum([2,5,5,11], 10)) == [1,2]
+    print(two_sum([2,7,11,15], 9)) #== [0, 1]
+    print(two_sum([3,2,4], 6)) #== [1,2]
+    print(two_sum([3,3], 6)) #== [0,1]
+    print(two_sum([2,5,5,11], 10)) #== [1,2]
